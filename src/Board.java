dc84dc40844981c35ba3788a830a5f78b60fc0ee
BT_1N3_28 <sample@bths.edu>
2024-03-15 09:26:49 -0400

    board class creation

--- src/Board.java
+++ src/Board.java
@@ -13,18 +13,7 @@ public class Board {
     }
 
 
-    public void setBoard() {
-        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
-        int num = 1;
-        int idx = 0;
-        for (String[] rows : board) {
-            for (String col : rows) {
-                col = letters[idx] + num;
-            }
-            num++;
-            idx++;
-        }
-    }
+
 }
 
 
--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
 public class Game {
+    private Scanner scanner;
+    private Board[][] board;
+
+    public Game() {
+        setBoard();
+        scanner  = new Scanner(System.in);
+    }
+
     public void setBoard() {
+
+    }
+
+    public void setCoord() {
         String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
         int num = 1;
         int idx = 0;
-        for (String[] rows : board) {
-            for (String col : rows) {
-                col = letters[idx] + num;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = letters[idx] + num;
             }
             num++;
             idx++;
         }
     }
+
+    private void printBoard() {
+        for (Board[] row : board) {
+            for (Board element : row) {
+                System.out.print(element.getSymbol());
+            }
+            System.out.println();
+        }
+    }
+
+
+
 }
