97ebbeec97a364250860f33daadd5efa634c28fd
BT_1N3_28 <sample@bths.edu>
2024-03-22 09:03:01 -0400

    board setup

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Game {
     private Scanner scanner;
     private Board[][] board;
+    private Player player;
+    private String playerLetter;
+    private int playerNum;
 
     public Game() {
         setBoard();
@@ -9,11 +12,18 @@ public class Game {
     }
 
     public void setBoard() {
-
+        board = new Board[10][10];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                Board x = new Board("\uD83C\uDF0A");
+                board[i][j] = x;
+            }
+        }
     }
 
     public void setCoord() {
         String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        int
         int num = 1;
         int idx = 0;
         for (int i = 0; i < board.length; i++) {
